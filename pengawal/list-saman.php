ecc08b347daf8846cbf7b6d15da00c9970b4722d
alifjamaluddin <alifjamaluddin92@gmail.com>
2015-07-08 08:33:15 +0800

    Update api for pengawal

--- pengawal/list-saman.php
+++ pengawal/list-saman.php
@@ -17,15 +17,25 @@ if (mysqli_connect_errno())
   $result['error'] = mysqli_connect_error();
 }else{
   $pelapor = $_GET['id'];
-  $ViewRS__query="SELECT * FROM `laporan` where pelapor = $pelapor";
+  $ViewRS__query="SELECT l.id, l.noplat, t.nama as 'tempat', l.catatan, l.nomatrik, u.nama as 'pesalah', k.nama as 'kesalahan', l.tarikhlaporan, p.nama as 'pelapor' 
+  FROM laporan l, kesalahan k, tempat t, pengawal p, user u
+  WHERE l.tempat = t.id 
+  AND l.kesalahan = k.id 
+  AND l.pelapor = p.id 
+  AND l.nomatrik = u.nomatrik
+  AND l.pelapor = $pelapor
+  ORDER BY l.tarikhlaporan DESC";
 
   $ViewRS = $connection->query($ViewRS__query);
   if ($ViewRS) {
     $result['status'] = "success";
     $listsaman = array();
+    $idsaman = array();
  	while($row = mysqli_fetch_assoc($ViewRS)){
  		array_push($listsaman, $row);
+    array_push($idsaman, $row['tarikhlaporan']);
  	}
+    $result['idsaman'] = $idsaman;
   	$result['listsaman'] = $listsaman;
 
   }
--- pengawal/pengumuman.php
+++ pengawal/pengumuman.php
@@ -16,12 +16,26 @@ if (mysqli_connect_errno())
   $result['status'] = "failed";
   $result['error'] = mysqli_connect_error();
 }else{
-  $ViewRS__query="SELECT * FROM `pengumuman` limit 5";
+  $ViewRS__query="SELECT * FROM pengumuman ORDER BY tarikh DESC";
 
   $ViewRS = $connection->query($ViewRS__query);
 
   if ($ViewRS) {
     $result['status'] = "success";
+    $tajuk = array();
+    $detail = array();
+    while($row = mysqli_fetch_assoc($ViewRS)){
+      array_push($tajuk, $row['tajuk']);
+      array_push($detail, $row);
+      // $tajuk[] = $row['tajuk']; 
+      // $detail[] = $row;
+    }
+   
+    $result['tajuk'] = $tajuk;
+    $result['detail'] = $detail;
+    
+
+    
   }
   else {
     $result['status'] = "failed";
